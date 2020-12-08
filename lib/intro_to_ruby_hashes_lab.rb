b800a2468beeb95d78d63de87fc0527d580574b2
William Bugenis <wbugenis@gmail.com>
2020-12-08 02:06:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,8 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  # retur
+  n a valid hash with any key/value pair of your choice
   {thing: 1}
 end
 
